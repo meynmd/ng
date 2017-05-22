17c4338dbae39363254e3997f7df93da75f4e3c7
matthew <meynm@oregonstate.edu>
2017-05-21 18:57:01 -0700

    smoothing

--- train-smooth-ngram.py
+++ train-smooth-ngram.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+from __future__ import division
 import sys
-import random
 import string
+import random
+import scipy.stats
+import math
 from collections import defaultdict
-from collections import Counter
 from itertools import product
+from copy import *
 
 
 def Load(filename):
@@ -14,59 +18,99 @@ def Load(filename):
     return fp.read()
 
 
-def Train(filename, order = 3, lid = 0.00001):
+def Train(filename, gram = 3):
     # make set of chars
     data = Load(filename)
     data = data.replace(' ', '_')
     data = data.replace('\n', '$')
-    chars = set([c for c in data])# if c != '\n'])
+    chars = set([c for c in data])
     data = string.lower(data)
-    data = (order - 1) * '#' + data
+    data = (gram - 1) * '#' + data
 
     # set up ngram dictionary with no nonzero values
     ngram = {}
     count = 0
-    pre = [''.join([x for x in p]) for p in product(chars, repeat = order - 1)]
-    pre += (order - 1) * '#'
-    for i in range(1, order):
-        pre += [i * '#' +
-            ''.join([x for x in p]) for p in product(chars, repeat = order - 1 - i)]
-    
+    pre = [''.join([
+        x for x in p]) for p in product(chars, repeat = gram - 1
+    )]
+    pre += (gram - 1) * '#'
+    for i in range(1, gram):
+        pre += [i * '#' + ''.join(
+            [x for x in p]
+        ) for p in product(chars, repeat = gram - 1 - i)]
+
     for p in pre:
-        ngram[p] = {x : lid for x in chars}
+        ngram[p] = {x : 0 for x in chars}
         count += len(chars)
 
     # count occurrences
-    for i in range(len(data) - order - 1):
-        sequence = data[i : i + order - 1]
-        nextChar = data[i + order - 1]
+    for i in range(len(data) - gram - 1):
+        sequence = data[i : i + gram - 1]
+        nextChar = data[i + gram - 1]
         ngram[sequence][nextChar] += 1
-    
+
+    smoothCount, totals = Smooth(ngram)
+
     # calculate probabilities
-    for seq, char_count in ngram.items():
-         n = sum(char_count.values()) + count * lid
-         for char, count in char_count.items():
-             ngram[seq][char] = float(count) / n
+    ngram = defaultdict(defaultdict)
+    for seq, char_count in smoothCount.items():
+        #n = sum(char_count.values())
+        for char, count in char_count.items():
+            ngram[seq][char] = float(count) / totals[seq]
+            if ngram[seq][char] > 1.:
+                print(seq, char, count, totals[seq], file=sys.stderr)
+
+    for p in pre:
+        for c in chars:
+            if c not in ngram[p]:
+                print(p, c, file=sys.stderr)
 
     return ngram
 
-    
-def ProbSmooth(ngram):
+
+def Smooth(ngramCount):
+    k = 1
     smoothedModel = {}
-    freq = defaultdict(int)
-    for seq, char_count in ngram:
-        for char, count in char_count:
+
+    # freq : occurrences of each count
+    freq = [0 for x in range(
+        int(max(max(cc.values()) for cc in ngramCount.values())) + 1)]
+    for seq, char_count in ngramCount.items():
+        for char, count in char_count.items():
             freq[count] += 1
 
-    for seq, char_count in ngram:
-        smoothedModel[seq] = {}
-        for char, count in char_count:
-            n = sum(char_count.values())
-            smoothedModel[seq][char] = 1 / n * (count + 1) * (freq[count + 1] / freq[count])
+    a, b, _, _, _ = scipy.stats.linregress(range(len(freq)), freq)
+
+    # smooth the counts
+    smoothCount = defaultdict(defaultdict)
+    totals = defaultdict(float)
+    for seq, char_count in ngramCount.items():
+        for char, count in char_count.items():
+            sc = 0
+            if count > 0:
+                sc = ngramCount[seq][char]
+            else:
+                nc = freq[count]
+                ncp1 = freq[count + 1]
+                if nc == 0 or ncp1 == 0:
+                    log_nc = b + a * math.log(count)
+                    sc = log_nc
+                else:
+                    nk = freq[1]
+                    nkp1 = freq[k + 1]
+                    sc = (
+                        (count + 1) * (float(ncp1) / nc) -
+                        count * (k + 1) * nkp1 / nk
+                        ) / (1 - (k + 1) * float(nkp1) / nk)
+
+            smoothCount[seq][char] = sc
+            totals[seq] += sc
+
+    return smoothCount, totals
 
 
 def MakeFSA(ngram, order, startSymbol = '<s>'):
-    fsa = 'F\n(S (S *e* 1.0))'
+    fsa = 'F\n(S (S *e* 1.0))\n'
     for seq, char_prob in ngram.items():
         for char, prob in char_prob.items():
             # if all chars are start symbol
@@ -76,7 +120,7 @@ def MakeFSA(ngram, order, startSymbol = '<s>'):
                 )
             else:
                 if char == '$':
-                    fsa += '({0} (F {1} {2}))'.format(
+                    fsa += '({0} (F {1} {2}))\n'.format(
                         seq, '</s>', prob
                     )
                 else:
@@ -84,12 +128,16 @@ def MakeFSA(ngram, order, startSymbol = '<s>'):
                         seq, seq[1 :] + char, char, prob
                     )
     return fsa
-   
-    
+
+
 
 if __name__ == "__main__":
-    order = int(sys.argv[2])
     filename = sys.argv[1]
+    order = int(sys.argv[2])
+    outfile = sys.stdout
+    if len(sys.argv) > 3:
+        outfile = open(sys.argv[3], 'w')
+
     model = Train(filename, order)
 
-    print MakeFSA(model, order)
+    print(MakeFSA(model, order), file=outfile)
